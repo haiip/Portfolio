de6af78640935dd01509f7a81995b5c63570929b
haiip <Nikita.magnusson@live.se>
2020-01-15 00:01:33 +0100

    v2

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,10 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
+import imgSrc from "./Bitemoji1.png";
 
 class Contacts extends Component {
   render() {
     return (
-      <div>
-        <h1> Contacts</h1>
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Nick Magnusson</h2>
+            <img src={imgSrc} alt="avatar" style={{ height: "250px" }} />
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    (+4673) 7738-450
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent
+                    style={{ fontSize: "30px", fontFamily: "Anton" }}
+                  >
+                    <i className="fa fa-envelope-square" aria-hidden="true" />
+                    nick.magnusson@student.kyh.se
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
